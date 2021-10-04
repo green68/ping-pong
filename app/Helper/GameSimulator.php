621a3963e9c5971a35477e8144ca37b853565558
green68 <eric.vercheval@hotmail.fr>
2021-10-04 13:43:20 +0200

    implementation similator

---
+++ app/Helper/GameSimulator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Helper;
+
+use App\Model\Game;
+use App\Model\Player;
+
+class GameSimulator {
+
+    private Game $game;
+    private array $players;
+
+    public function __construct(string $namePlayer1, string $namePlayer2)
+    {
+        $this->game = new Game(Game::MIN_SET_FOR_WIN, 2);
+        $this->players[] = new Player($namePlayer1);
+        $this->players[] = new Player($namePlayer2);
+        $this->game->addPlayer($this->players[0]);
+        $this->game->addPlayer($this->players[1]);
+        $this->gameStart();
+        $this->gamePlay();
+        $this->gameFinished();
+
+    }
+
+    private function gameStart()
+    {
+        echo "<h1>Game : {$this->game->getId()}</h1>";
+        echo "<h2>Joueur 1: ". $this->game->getPlayers()[0]->getName()."<br>";
+        echo "Joueur 2: ". $this->game->getPlayers()[1]->getName()."</h2>";
+        $this->game->start();
+    }
+
+
+    private function randomPlayerNum() {
+        return random_int(0,1);
+    }
+
+    private function gamePlay(){
+
+        do {
+            $this->game->getCurrentSet()->addPointToPlayerNum($this->randomPlayerNum());
+            echo $this->game->getCurrentSet()->getScore()->getPoints(0)." - ".$this->game->getCurrentSet()->getScore()->getPoints(1)." / ";
+            // control des sets gagnants
+            if($this->game->getCurrentSet()->isWinner()) {
+                echo "<h3>Set gagné par ".$this->game->getCurrentSet()->getWinner()->getName()."</h3><hr>";
+                // ajout d'un set?
+                if(!$this->game->isGameWin()){
+                    $this->game->addGameSet();
+                    echo "<h2>Set : " . count($this->game->getSets()) . "</h2>";
+                }
+            }
+    
+        } while (!$this->game->isGameWin());
+    }
+
+    private function gameFinished(){
+        // recup nbre sets pour chaque joueur
+        $sets = array(0,0);
+        foreach ($this->game->getSets() as $set) {
+            $set->getWinner() === $this->players[0] ? $sets[0]++ : $sets[1]++;
+        } 
+        echo "<h2>Jeu gagné par ".$this->game->getWinner()->getName()." : $sets[0] / $sets[1]</h2>";
+    }
+}
--- index.php
+++ index.php
@@ -1,58 +1,8 @@
 <?php
     require 'vendor/autoload.php'; 
     
-    use App\Model\Game;
-    use App\Model\Player;
+    use App\Helper\GameSimulator;
 
-    function randomPlayerNum() {
-        return random_int(0,1);
-    }
+    $gameSimulator = new GameSimulator("Eric", "David");
 
-    const NUMBER_OF_PLAYERS = 2;
-
-    $game = new Game(Game::MIN_SET_FOR_WIN, NUMBER_OF_PLAYERS);
-
-    try {
-        // instance joueur1 et joueur2, puis affectation à un match
-        $playerOne = new Player("Eric");
-        $playerTwo = new Player("Kevin");
-
-        $game->addPlayer($playerOne);
-        $game->addPlayer($playerTwo);
-        // instance joueur3 et tentative ajout à un match
-        // $game->addPlayer($playerThree);
-    } catch (\Throwable $th) {
-        echo "<br>".$th->getMessage();
-    }
-
-    // démarrage match
-    echo "<h1>Game : {$game->getId()}</h1>";
-    echo "<h2>Joueur 1: ". $game->getPlayers()[0]->getName()."<br>";
-    echo "Joueur 2: ". $game->getPlayers()[1]->getName()."</h2>";
-
-    $game->start();
-    echo "<h2>Set : " . count($game->getSets()) . "</h2>";
-    
-    do {
-        $game->getCurrentSet()->addPointToPlayerNum(randomPlayerNum());
-        echo $game->getCurrentSet()->getScore()->getPoints(0)." - ".$game->getCurrentSet()->getScore()->getPoints(1)." / ";
-        // control des sets gagnants
-        if($game->getCurrentSet()->isWinner()) {
-            echo "<h3>Set gagné par ".$game->getCurrentSet()->getWinner()->getName()."</h3><hr>";
-            // ajout d'un set?
-            if(!$game->isGameWin()){
-                $game->addGameSet();
-                echo "<h2>Set : " . count($game->getSets()) . "</h2>";
-            }
-        }
-
-    } while (!$game->isGameWin());
-
-    // recup nbre sets pour chaque joueur
-    $sets = array(0,0);
-    foreach ($game->getSets() as $set) {
-        $set->getWinner() === $playerOne ? $sets[0]++ : $sets[1]++;
-    } 
-    dump($sets);
-    echo "<h2>Jeu gagné par : ".$game->getWinner()->getName()." $sets[0] / $sets[1]</h2>";
     
